67c55518daa97b29e95fe1c3daa1ec3cb76d6357
SeanTylerLee <seantylerlee@outlook.com>
2026-03-11 14:27:08 -0500

    Change TO_EMAIL to permitpath@icloud.com

--- api/contact.js
+++ api/contact.js
@@ -4,7 +4,7 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // TEMP: send to an email you can check easily (e.g. Gmail)
-const TO_EMAIL = "support@permitpathnav.com";
+const TO_EMAIL = "permitpath@icloud.com";
 // Use Resend's default verified sender for now
 const FROM_EMAIL = "Acme <onboarding@resend.dev>";
 
